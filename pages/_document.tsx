c66eddaa876acdc2656e64fe9cfb1a72ce54d52f
Karol Binkowski <starystachu9@gmail.com>
2022-11-20 15:44:17 +0100

    ignore react/no-danger eslint warning

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           />
+          {/* eslint-disable react/no-danger */}
           <script
             dangerouslySetInnerHTML={{
               __html: `
